8604f54fa5f09c23f4e779a31532e6d4931a9b6a
jabeca <abeciaj23@gmail.com>
2021-05-21 15:25:39 +0800

    Added menu list and installation commands

--- installation.sh
+++ installation.sh
@@ -1,15 +1,69 @@
-#!/bin/sh
+#!/bin/bash
 
-#Stop automatic updater
-sudo dpkg-reconfigure -plow unattended-upgrades
+apps=("skype" "slack" "google-chrome")
 
-#Reconfigure unpacked packages
-sudo dpkg --configure -a
+#Select menu
+PS3="Select action: "
+select opt in "Update" "Update & Install" "Quit"
+do
+  case $opt in
+    "Update")
 
-#Update system
-sudo apt update && sudo apt -f install && sudo apt full-upgrade
+      #Stop automatic updater
+      sudo dpkg-reconfigure -plow unattended-upgrades
 
-#Enable automatic updater
-sudo dpkg-reconfigure -plow unattended-upgrades
+      #Reconfigure unpacked packages
+      sudo dpkg --configure -a
 
+      #Update system
+      sudo apt update && sudo apt -f install && sudo apt full-upgrade
+
+      #Enable automatic updater
+      sudo dpkg-reconfigure -plow unattended-upgrades
+      break;;
+
+    "Update & Install")
+
+      #Stop automatic updater
+      sudo dpkg-reconfigure -plow unattended-upgrades
+
+      #Reconfigure unpacked packages
+      sudo dpkg --configure -a
+
+      #Update system
+      sudo apt update && sudo apt -f install && sudo apt full-upgrade
+
+      #Enable automatic updater
+      sudo dpkg-reconfigure -plow unattended-upgrades
+
+      #Array loop
+      for i in ${apps[@]}
+      do
+	#Check if app is installed
+        if ! command -v $i &> /dev/null
+        then
+
+	  #If value is google-chrome
+          if [ "$i" == "google-chrome" ]
+          then
+
+	    #Google chrome installation
+            wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+            sudo apt install ./google-chrome-stable_current_amd64.deb
+          else
+	    #Install from snap
+            sudo snap install $i --classic
+          fi
+    	fi
+      done
+      break;;
+
+    "Quit")
+      break;;
+
+    #If input is invalid
+    *) echo "$REPLY is invalid";;
+
+  esac
+done
 
